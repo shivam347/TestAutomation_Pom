93b598145c0319ef54dc3aacedcffa3931cb0c71
Shivam <shivamfeb77@gmail.com>
2026-01-18 15:09:11 +0530

    added typos test cases

---
+++ src/test/java/herokuapp/pages/TyposPage.java
@@ -0,0 +1,28 @@
+package herokuapp.pages;
+
+import herokuapp.util.WaitUtil;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
+import org.testng.log4testng.Logger;
+
+public class TyposPage {
+
+    private static final Logger log = Logger.getLogger(TyposPage.class);
+    private WebDriver driver;
+
+    public TyposPage(WebDriver driver) {
+        Reporter.log("Initializing TyposPage", true);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(css = "div.example p")
+    private WebElement paragraph;
+
+    public String getParagraphText() {
+        return WaitUtil.waitForVisibility(driver, paragraph).getText();
+    }
+}
